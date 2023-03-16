4afdbdd2973d51ef25ce8a73bc10d09741933c34
Maryam Fakhraei <M.Fakhraeisari@gmail.com>
2023-03-16 11:02:08 +0330

    Define A Function To Check Queen Position

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 class ChessBoard:
     def __init__(self, n):
-        self.n = n 
+        self.n = n
         self.queen_place_columns = []
 
     '''
@@ -23,5 +23,28 @@ class ChessBoard:
     queen_place_columns = [2, 0, 3, 1]
     '''
 
+    def is_this_column_safe_in_next_row(self, column):
+        # index of next row
+        row = len(self.queen_place_columns)
+
+        # check column
+        for queen_column in self.queen_place_columns:
+            if column == queen_column:
+                return False
+
+        # check diagonal
+        for queen_row, queen_column in enumerate(self.queen_place_columns):
+            if queen_column - queen_row == column - row:
+                return False
+
+        # check other diagonal
+        for queen_row, queen_column in enumerate(self.queen_place_columns):
+            if ((self.n - queen_column) - queen_row == (self.n - column) - row):
+                return False
+        return True
+
+
 size = int(input('Enter queen count : '))
 board = ChessBoard(size)
+
+status = board.is_this_column_safe_in_next_row()
